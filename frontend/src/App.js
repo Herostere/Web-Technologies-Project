ea7e9d1cfc88fa46719efc7ce92fb11ec64a318f
DanStudent <danial.joukar@student.umons.ac.be>
2022-07-24 12:10:45 +0200

    Implemented homepage :)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import NavBar from './Components/NavBar';
-import {BrowserRouter as Router} from 'react-router-dom'
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import HomePage from './Components/HomePage'
 function App() {
   return (
-
     <Router>
       <NavBar/>
-      
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
     </Router>
+    
   );
 }
 

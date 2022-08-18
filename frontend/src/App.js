d9c6f3547cdf633f9713d4705d40875e766cc66d
DanStudent <danial.joukar@student.umons.ac.be>
2022-08-18 15:01:04 +0200

    added product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './Components/HomePage'
+import ProductsPage from './Components/ProductsPage';
 function App() {
   return (
     <Router>
       <NavBar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/Products' element={<ProductsPage/>}/>
       </Routes>
     </Router>
     
---
+++ frontend/src/Components/ProductsPage/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './ProductsPage.css'
+const ProductsPage = () => {
+  return (
+    <div className='productspage'>
+        <div className='title'>
+            <h2>Products</h2>
+        </div>
+        <div className='product-container'>
+            <div className='cleo'>
+              <h3>CLEO</h3>
+              <ul>
+                <li>Robust analysis of bones from 2D radiographs or 3D scans</li>
+                <li>Several parameters of bone density and microarchitecture</li>
+                <li>Reconstruction of the 3D bone density and microarchitecture from 2D radiographs</li>
+                <li>Efficient bone diagnosis related to osteoporosis diseases</li>
+              </ul>
+            </div>
+            <div className='cles'>
+              <h3>CLES</h3>
+              <ul>
+                <li>Three-dimensional spine reconstruction from 2D radiographs</li>
+                <li>Fully automatic vertebra detection, segmentation and angular measurement from X-Ray images</li>
+                <li>3D vertebra detection and segmentation from MR images</li>
+              </ul>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default ProductsPage;
